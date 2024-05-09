1ab9abe3904900d8a6df0fd7ad6f7b4e610ff168
Ajinkya Tadas <124388761+AjinkyaTadas@users.noreply.github.com>
2024-05-09 22:03:49 +0530

    bootstrap cards with map method

---
+++ src/Data.js
@@ -0,0 +1,28 @@
+
+const Data = [
+    {
+        imgsrc: "https://filmfare.wwmindia.com/content/2023/dec/shahrukhkhan11703243433.jpg",
+        title: "Shah Rukh Khan",
+        text: "Shah Rukh Khan, also known by the initialism SRK, is an Indian actor and film producer who works in Hindi films. Referred to in the media as the Baadshah.",
+        btn: "Read more...",
+    },
+    {
+        imgsrc: "https://img.etimg.com/thumb/width-1200,height-900,imgsize-117330,resizemode-75,msid-85097207/magazines/panache/tiger-shroff-to-release-new-patriotic-song-vande-mataram-ahead-of-independence-day.jpg",
+        title: "Tiger Shroff",
+        text: "Jai Hemant Shroff, known professionally as Tiger Shroff, is an Indian actor who works in Hindi films. Born to actors Jackie Shroff and Ayesha Dutt.",
+        btn: "Read more...",
+    },
+    {
+        imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqB4vuz4VofoXmQe9KqDuJ3Iv6Pm1r42kuhhXW7Qp0vQ&s",
+        title: "Salman Khan",
+        text: "Abdul Rashid Salim Salman Khan is an Indian actor, film producer, and television personality who works predominantly in Hindi films.",
+        btn: "Read more...",
+    },
+    {
+        imgsrc: "https://goldenglobes.com/wp-content/uploads/2023/11/01-aamir-khan-gettyimages-669219114-2017.jpg",
+        title: "Aamir Khan",
+        text: "Mohammed Aamir Hussain Khan is an Indian actor, filmmaker, and television personality who works in Hindi films.",
+        btn: "Read more...",
+    }
+]
+export default Data
